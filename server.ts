5947b72267b81b72495d6cad9babb27fe132be2c
Dave Battiston <davebattiston@Daves-MacBook-Pro.local>
2024-10-09 13:35:38 -0400

    remove lock

--- server.ts
+++ server.ts
@@ -1,10 +1,15 @@
 import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
+import Contact from "./components/contact.tsx";
 
 const app = new Hono();
 
 app.use("/src/*", serveStatic({ root: "./" }));
 
 app.get("/", serveStatic({ root: "./src" }));
+
+app.get("/contact", (c) => {
+  return c.html(Contact);
+});
 
 Deno.serve(app.fetch);
