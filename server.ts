18c58f1e428475cd417f02eba3c2a907d5fa9b7f
Dave Battiston <davebattiston@Daves-MacBook-Pro.local>
2024-10-13 00:05:35 -0400

    added htmx and hyperscript

--- server.ts
+++ server.ts
@@ -1,15 +1,12 @@
 import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
-import Contact from "./components/contact.tsx";
 
 const app = new Hono();
 
-app.use("/src/*", serveStatic({ root: "./" }));
-
-app.get("/", serveStatic({ root: "./src" }));
-
-app.get("/contact", (c) => {
-  return c.html(Contact);
-});
+app.use("/*", serveStatic({ root: "./static" }));
+app.get(
+  "/",
+  serveStatic({ root: "./" }),
+);
 
 Deno.serve(app.fetch);
